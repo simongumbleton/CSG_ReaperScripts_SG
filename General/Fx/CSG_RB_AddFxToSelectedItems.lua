9ada483341fdc2550ab10e4ccf5e63c56dace897
simongumbleton <simongumbleton@gmail.com>
2020-04-21 16:04:32 +0100

    Fix file type

---
+++ General/Fx/CSG_RB_AddFxToSelectedItems.lua
@@ -0,0 +1,15 @@
+--[[
+* ReaScript Name: CSG_RB_AddFxToSelectedItems
+* Description: 
+* Instructions: 
+* Author: Rob Blake
+* Version: 1.01
+--]]
+
+
+all_items = reaper.CountSelectedMediaItems(0)
+  for i = 0, all_items-1 do
+    item =  reaper.GetSelectedMediaItem(0,i)
+    MediaItem_Take = reaper.GetTake(item, 0)
+    reaper.TakeFX_AddByName(MediaItem_Take, "ReaComp (Cockos)", 1) -- Enter plugin name here
+  end
